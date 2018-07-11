1e157b68964b2beb8e173cb73b93ae2225034554
Gavin Soko <sokolofg@gmail.com>
2018-07-11 12:46:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ function theBeatlesPlay(music, instrument){
 }
 return musical;
   }
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
